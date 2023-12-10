2a9167adf17211bd7eb7f4f0a8acc0252c3dadcb
Ahmed Fekry <ahmedfekry792@gmail.com>
2023-12-10 14:00:29 +0200

    uploading files

---
+++ HelloITI.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello ITI"); 
+    }
+    public void printHelloGit(){
+    	System.out.println("Hello Git");
+    }
+}
